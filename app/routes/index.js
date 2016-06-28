42feaf229e8391e86b4270b4a33255c64b290ed7
Andrew Boyle <ajboyle31@gmail.com>
2016-06-28 00:32:45 +0000

    commented out partials code, added newpoll and process_get routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -17,11 +17,14 @@ module.exports = function (app, passport) {
 	//will call the getPolls function if the polls partials function is on the page being rendered
 	//need to create a getPolls funciton which will access the database and load each of the polls on the database
 	//this will happen automatically on page load so do not need to change clickHandler or clickController
+	//THIS DOES NOT WORK
+	/*
 	app.use(function(req, res, next){
     	if(!res.locals.partials) res.locals.partials = {};
     	res.locals.partials.polls = getPolls.returnPolls();
     	next();
 	});
+	*/
 
 	//what is this?
 	var clickHandler = new ClickHandler();	
@@ -46,11 +49,36 @@ module.exports = function (app, passport) {
 		});
 
 	//if you are logged in, you can access the profile page
+	//TO BE DELETED, THERE WILL BE NO PROFILE PAGE
 	app.route('/profile')
 		.get(isLoggedIn, function (req, res) {
 			res.render('profile');
 		});
-
+	
+	//if you are logged in and want to create a new poll	
+	app.route('/newpoll')
+		.get( function (req, res){
+			res.render('newpoll');	
+		});
+		
+	//for testing of new poll data entry
+	app.route('/process_GET')
+		.get(function (req, res){
+			var response = {
+				pollname:req.query.pollname,
+				options:req.query.options
+			};
+			res.end(JSON.stringify(response));
+		});
+	
+	
+	
+	//if you are logged in and want to see all of the polls you created
+	app.route('/mypolls')
+		.get(isLoggedIn, function (req, res){
+			res.render('mypolls');
+		});
+	
 	//gets user information from github
 	app.route('/api/:id')
 		.get(isLoggedIn, function (req, res) {
